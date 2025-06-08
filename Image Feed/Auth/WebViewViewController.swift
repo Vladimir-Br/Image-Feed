e40eff538c4951c503854e6d096b02256a71a8b3
Vladimir-Br <briukovkin@yandex.ru>
2025-06-08 11:58:55 +0300

    добавлен URLComponents

--- Image Feed/Auth/WebViewViewController.swift
+++ Image Feed/Auth/WebViewViewController.swift
@@ -1,9 +1,40 @@
-
-
 import UIKit
 import WebKit
+
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
 
 class WebViewViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .darkContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAuthView()
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            return
+        }
+
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+
+        guard let url = urlComponents.url else {
+            return
+        }
+
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
--- Image Feed/Constants.swift
+++ Image Feed/Constants.swift
@@ -4,7 +4,7 @@ import Foundation
 enum Constants {
     static let accessKey = "jhr-4XeTfbS4OpZYUMB6bLsdvUdkOVqDJQVszZvcMrs"
     static let secretKey = "6Zok4zcOSpexUIXNnCTEHYT2SKfKO2Ekx4ElDviP0P8"
-    static let redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
     
